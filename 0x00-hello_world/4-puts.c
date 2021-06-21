3fb924fc1842fcd3ceb7b131228bfc4ca7ebd297
knightiti <ktess2008@gmail.com>
2021-06-21 04:12:49 -0700

    <Hello world C program>

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 int main(void)
+/**
+ * main - Entry point
+ * print message
+ * Return: Always 0 (Success)
+ */
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
