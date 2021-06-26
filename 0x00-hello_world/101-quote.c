e243e8268e03ff1130bc4fd9d4e8e6145c9edbf5
knightiti <ktess2008@gmail.com>
2021-06-26 11:58:47 -0700

    <Alternative to printf and puts>

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 #include <unistd.h>
 /**
  * main - Entry point
- * printf alternative
- * Return: Always 0 (Success)
+ * printing to the standard error
+ * Return: 1 (Success)
  */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
 return (1);
 }
