95dcaf79159cf67fb23922cf05c19b2ffc08dca7
knightiti <ktess2008@gmail.com>
2021-06-26 10:49:50 -0700

    <perfected size of variables>

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ int i;
 long int il;
 long long int ill;
 float f;
-printf("Size of a char: %lu bytes(s)\n", (unsigned long)sizeof(c));
-printf("Size of an int: %lu bytes(s)\n", (unsigned long)sizeof(i));
-printf("Size of a long int: %lu bytes(s)\n", (unsigned long)sizeof(il));
-printf("Size of a long long int: %lu(bytes)(s)\n", (unsigned long)sizeof(ill));
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(il));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(ill));
 printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
