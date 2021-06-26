e7dd89c611bd3c0569da08421c192a50dbd0bbf9
knightiti <ktess2008@gmail.com>
2021-06-26 10:36:52 -0700

    <compiling size of variables>

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,15 @@
  */
 int main(void)
 {
-printf("Size of a char: %ld byte(s)\n", sizeof(char));
-printf("Size of an int: %ld byte(s)\n", sizeof(int));
-printf("Size of a long int: %ld byte(s)\n", sizeof(long));
-printf("Size of a long long int: %ld byte(s)\n", sizeof(long long));
-printf("Size of a float: %ld byte(s)\n", sizeof(float));
+char c;
+int i;
+long int il;
+long long int ill;
+float f;
+printf("Size of a char: %lu.\n", (unsigned long)sizeof(c));
+printf("Size of an int: %lu.\n", (unsigned long)sizeof(i));
+printf("Size of a long int: %lu.\n", (unsigned long)sizeof(il));
+printf("Size of a long long int: %lu.\n", (unsigned long)sizeof(ill));
+printf("Size of a float: %ld byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
