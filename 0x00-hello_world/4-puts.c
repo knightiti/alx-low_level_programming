11df73eebdab4dd9defa6d5e75328c95135b4d2d
knightiti <ktess2008@gmail.com>
2021-06-26 06:48:35 -0700

    <playing around comments>

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,7 @@
 #include <stdio.h>
-/**
- * main - Entry point
- * print message
- * Return: Always 0 (Success)
- */
+
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+puts("Programming is like building a multilingual puzzle");
 return (0);
 }
