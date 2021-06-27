ff10a921b5b333f1752338141ce0bd1ab8633eb6
knightiti <ktess2008@gmail.com>
2021-06-27 14:40:35 -0700

    <Fixed character checks>

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * _islower - Entry Point
+ * @c: Check for lower case character
+ * Return: 1 if int c is lowercase
+ * Return: 0 if otherwise
+ */
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+return (1);
+else
+return (0);
+}
