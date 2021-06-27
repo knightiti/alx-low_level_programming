38a5dda4997b301588c1c8b82200b1a5f1bac216
knightiti <ktess2008@gmail.com>
2021-06-27 05:37:37 -0700

    <Unbeatable combinations for success>

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 /**
  * main - Entry point
- *
- *Return: Always 0 (Success)
+ * printing possible combinationams of single digit numbers
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 int i;
-
 for (i = 0; i < 10; i++)
 {
 putchar(i + '0');
