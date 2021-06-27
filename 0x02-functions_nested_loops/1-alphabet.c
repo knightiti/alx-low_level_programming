dfaf3847944f4a0f3f5e306562b8957036f43761
knightiti <ktess2008@gmail.com>
2021-06-27 12:50:33 -0700

    <Printing lowercase alphabets>

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * main - entry point
+ * print lower case alphabet
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+int i;
+
+for (i = 'a'; i <= 'z'; i++)
+_putchar(i);
+_putchar('\n');
+}
