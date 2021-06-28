1d16929999938b431f29efe93295e4010053644d
knightiti <ktess2008@gmail.com>
2021-06-27 19:47:09 -0700

    <Wrapping up mandatory tasks on function>

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * add - entry point
+ * @x: number to be added
+ * @y: number to be added
+ * Return: Always 0 (Success)
+ */
+int add(int x, int y)
+{
+return (x + y);
+}
