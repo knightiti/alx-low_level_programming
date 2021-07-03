156bd3e6707e244feb9188c19219764782464f0e
knightiti <ktess2008@gmail.com>
2021-07-03 08:17:23 -0700

    <Getting a hang on functions>

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * print_times_table - Entry point
+ * @n: input
+ * Return:  void 
+ */
+void print_times_table(int n)
+{
+if (n <= 15 && n >= 0)
+_putchar('n');
+_putchar('\n');
+
+}
