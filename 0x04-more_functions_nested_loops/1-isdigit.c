05b7b5a9962eaac85ea3c7151fca78ea0d060fbe
knightiti <ktess2008@gmail.com>
2021-07-04 03:02:03 -0700

    <Stil on functions>

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,7 +2,7 @@
 /**
  * _isdigit - a function that checks for digit
  * @c: digit 0 to 9
- * Return : 1, if (c = '0'), 0 if otherwise.
+ * Return: 1 if c is a digit, 0 if otherwise.
  */
 
 int _isdigit(int c)
--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -3,7 +3,7 @@
  * mul - a function that multiplies two integers
  * @a: 1st integer
  * @b: 2nd integer
- * Return : Always 0.
+ * Return: Always 0.
  */
 
 int mul (int a, int b)
