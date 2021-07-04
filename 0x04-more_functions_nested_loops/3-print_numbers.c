00e0540497bbe39d04cfd514e2a2f5b24b671c27
knightiti <ktess2008@gmail.com>
2021-07-04 03:48:45 -0700

    <The numbers speak for themselves>

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,17 @@
 #include "holberton.h"
 /**
  * print_numbers - A function that print numbers
- * @n: number to be printed
- * Return: (0)
+ *
+ * Return: void
  */
 
-void print_numbers (void)
+void print_numbers(void)
 {
 int n;
 
 for (n = 0; n < 10; n++)
 {
-_putchar(n +'0');
+_putchar(n + '0');
 }
 _putchar('\n');
 }
