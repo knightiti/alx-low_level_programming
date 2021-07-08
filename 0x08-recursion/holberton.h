dec0bb4f783ee9acab3a379d44fdf7c6d82f2bd6
knightiti <ktess2008@gmail.com>
2021-07-08 13:56:51 -0700

    <Recursion>

--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -1,4 +1,5 @@
-int _putchar(char c);
+
+
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
@@ -8,3 +9,4 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
+int _putchar(char c);
