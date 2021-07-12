27fb7f52db4aa3bbeec6a3c8943d7f4ae27c5988
knightiti <ktess2008@gmail.com>
2021-07-12 12:15:32 -0700

    <First commit>

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * main - entry point
+ * @argc: number of command line arguments
+ * @argv: array containing the command line arguement
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+argc = 0;
+
+printf("%s\n", argv[argc]);
+return (0);
+}
