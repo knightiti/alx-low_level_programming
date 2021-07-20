253b9a70beb0e21e53d2f646b9915ebaf4f5d274
knightiti <ktess2008@gmail.com>
2021-07-19 19:41:50 -0700

    <Puppy>

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,16 +7,13 @@
  * @age: age of dog
  * @owner: owner of dog
  *
- * Description: a structure that defines the name, age and owner of a dog
  */
 
-typedef struct dog
+struct dog
 {
 char* name;
 float age;
 char* owner;
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
 }
 #endif /*DOG_H*/
