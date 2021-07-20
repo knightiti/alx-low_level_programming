42fc410773c8875d577bbbb0a8e0693504219a83
knightiti <ktess2008@gmail.com>
2021-07-19 18:45:43 -0700

    <Structures>

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG.H
+#define DOG.H
+
+/**
+ * struct dog - structure
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Description: a structure that defines the name, age and owner of a dog
+ */
+
+struct dog
+{
+char* name;
+float age;
+char* owner;
+}
+#endif /*DOG.H*/
