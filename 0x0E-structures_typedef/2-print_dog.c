5cdf669085106146f5b72739026034f69064dc63
knightiti <ktess2008@gmail.com>
2021-07-20 05:05:11 -0700

    <Unconditional love>

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,16 +9,14 @@
 
 void print_dog(struct dog *d)
 {
-  
+if (d == NULL)
+return;
 if (d->name == NULL)
 d->name = "(nil)";
 if (d->age <= 0)
 printf("Age: (nil)\n");
 if (d->owner == NULL)
 d->owner = "(nil)";
-
-if (d == NULL)
-return;
 
 printf("Name: %s\n", d->name);
 printf("Age: %f\n", d->age);
