7088a59143cda50f4ffd77119223cbb5a7822837
knightiti <ktess2008@gmail.com>
2021-07-19 19:18:02 -0700

    <Puppy>

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -10,10 +10,18 @@
  * Description: a structure that defines the name, age and owner of a dog
  */
 
-typedef struct dog
+struct dog
 {
 char* name;
 float age;
 char* owner;
+};
+int main(void)
+{
+struct dog my_dog;
+my_dog.name = "poppy";
+my_dog.age = "3.5";
+my_dog.owner = "Bob";
+return 0;
 }
 #endif /*DOG_H*/
