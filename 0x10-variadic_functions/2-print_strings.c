33762e3736d2e8a03ac2ca8efa7b2f9bbca09dd4
knightiti <ktess2008@gmail.com>
2021-07-22 13:13:16 -0700

    <Fixed betty coding style>

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -15,9 +15,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
   va_list ap;
   unsigned int i;
   char *str;
-
 va_start(ap, n);
-
 for (i = 0; i < n; i++)
 {
 str = va_arg(ap, char *);
@@ -28,7 +26,6 @@ printf("(nil)");
 if (separator != NULL && i < n - 1)
 printf("%s", separator);
 }
- 
 printf("\n");
 va_end(ap);
 }
