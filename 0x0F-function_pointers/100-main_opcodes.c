664a9c89e40b82d1963ad745ad544b9a78ba5009
knightiti <ktess2008@gmail.com>
2021-07-23 15:12:23 -0700

    <Fixed code error>

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -9,27 +9,30 @@
  */
 int main(int argc, char *argv[])
 {
-int x, i;
-unsigned char *p;
+int i, value;
 
 if (argc != 2)
+
 {
 printf("Error\n");
 exit(1);
 }
-x = atoi(argv[1]);
-if (x < 0)
+value = atoi(argv[1]);
+
+if (value < 0)
 {
 printf("Error\n");
 exit(2);
 }
-p = (unsigned char *)main;
-i = 0;
-if (x > 0)
+
+for (i = 0; i < value; i++)
 {
-while (i < (x - 1))
-printf("%02hhx ", p[i++]);
-printf("%hhx\n", p[i]);
+printf("%02hhx", *((unsigned char *) main + i));
+if (i < value - 1)
+printf(" ");
+else
+printf("\n");
 }
 return (0);
 }
+
