2bf6c482d510f8191b7a7f566da9dd518cbfd52a
knightiti <ktess2008@gmail.com>
2021-08-04 12:22:58 -0700

    <Fixed betty coding style>

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,11 +11,11 @@ size_t print_listint(const listint_t *h)
 int i;
 
 if (h == NULL)
-return (0);
+return(0);
 for (i = 0; h; i++)
 {
 printf("%d\n", h->n);
 h = h->next;
 }
-return (i);
+return(i);
 }
