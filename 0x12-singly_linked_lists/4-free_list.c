5db44b8576df81512e9b595858bf1dba54994175
knightiti <ktess2008@gmail.com>
2021-08-07 10:56:54 -0700

    <Fixed code>

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,26 +1,17 @@
 #include "lists.h"
 
 /**
- * r_free_list - a func that frees malloc'ed memory recursively
- * @ptr: pointer to the linked list
- **/
-
-void r_free_list(list_t *ptr)
-{
-if (ptr->next != NULL)
-r_free_list(ptr->next);
-free(ptr->str);
-free(ptr);
-}
-
-/**
- * free_list - frees all malloc'ed memory associated with a linked list
+ * free_list - a function that frees a list_t list.
  * @head: pointer to head of linked list
+ * Return: void
  */
 
 void free_list(list_t *head)
 {
 if (head == NULL)
 return;
-r_free_list(head);
+if (head->next != NULL)
+free_list(head->next);
+free(head->str);
+free(head);
 }
