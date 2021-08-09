acf855379ad31f9836dfb08463c5e7377c3b10cf
knightiti <ktess2008@gmail.com>
2021-08-09 10:11:47 -0700

    <Creating a file>

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * create_file - a func that creates a file
+ * @filename: name of file to be created
+ * @text_content: NULL terminated string to write to the file
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+int file, f = 0;
+
+if (filename == NULL)
+return (-1);
+
+file = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+if (file == -1)
+return (-1);
+
+while (text_content[f])
+f++;
+if (text_content == NULL)
+{
+close(file);
+return (-1);
+}
+else
+{
+write(file, text_content, f);
+}
+
+close(file);
+return (1);
+}
